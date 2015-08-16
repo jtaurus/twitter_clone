7f6ba2320702a0df3294b2b372e7797736924d49
Jakub <truefear@gmail.com>
2015-08-16 14:26:16 +0200

    Fix typo.

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'root',
+			'database'  => 'twitter_clone',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
--- app/database/migrations/2015_08_16_121419_create_users_table.php
+++ app/database/migrations/2015_08_16_121419_create_users_table.php
@@ -29,7 +29,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop("Users")l
+		Schema::drop("Users");
 	}
 
 }
