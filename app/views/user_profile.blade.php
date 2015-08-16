4c2d6fe60bad7e425ca1fa49c557e77c0132f489
Jakub <truefear@gmail.com>
2015-08-16 16:00:04 +0200

    Create partial for navbar

--- app/views/user_profile.blade.php
+++ app/views/user_profile.blade.php
@@ -4,7 +4,38 @@ Viewing profile: {{$data["user"]->username}}
 @stop
 @section('content')
 Viewing profile of user: {{$data["user"]->username}}
-@foreach($data["messages"] as $oneMessage)
-	<p>{{$oneMessage->message_body}}</p><br />
-@endforeach
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+		@include('partials.navbar')
+			<div class="row">
+				<div class="col-md-3">
+					<div class="row">
+						<div class="col-md-12">
+							User Details:
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							Recent followings:
+						</div>
+					</div>
+				</div>
+				<div class="col-md-9">
+					User messages:
+					<div class="row">
+						<div class="col-md-12">
+							One message
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					The footer
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @stop
