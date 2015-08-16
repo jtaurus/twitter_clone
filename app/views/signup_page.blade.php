33181063d2fa666ecdaf88215003242dcdd9f4e4
Jakub <truefear@gmail.com>
2015-08-16 16:47:43 +0200

    Add sign up form.

---
+++ app/views/signup_page.blade.php
@@ -0,0 +1,30 @@
+@extends('master.main_layout')
+@section('title')
+Sign up
+@stop
+
+@section('content')
+<center>Create a new account</center>
+<div class="col-md-4">
+</div>
+<div class="col-md-4">
+{{Form::open();}}
+<div class="form-group">
+{{Form::label('username', 'Username');}}
+{{Form::text('username', 'username', array('class' => 'form-control'));}}
+</div>
+<div class="form-group">
+{{Form::label('email', 'E-mail')}}
+{{Form::text('email', 'email', array('class' => 'form-control', 'type' => 'email', 'placeholder' => 'email'));}}
+</div>
+<div class="form-group">
+{{Form::label('password', 'Password');}}
+{{Form::password('password', array('class' => 'form-control'));}}
+</div>
+<br />
+{{Form::submit('Submit', array('class' => 'btn btn-default'));}}
+{{Form::close();}}
+</div>
+<div class="col-md-4">
+</div>
+@stop
