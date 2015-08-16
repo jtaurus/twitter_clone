be7de637a647c749c5424f32afab5fff2ec9244a
Jakub <truefear@gmail.com>
2015-08-16 16:36:40 +0200

    Create routes for signup pages

--- app/routes.php
+++ app/routes.php
@@ -11,6 +11,13 @@
 |
 */
 
+// Home page
 Route::get('/', ['as' => 'homepage', 'uses' => 'HomeController@display_homepage']);
 
+// View users profile
 Route::get('/user/{username}', ['as' => 'user_profile', 'uses' => 'UserController@display_user_profile']);
+
+Route::get('/signup', ['as' => 'signup_page', 'uses' => 'UserController@display_signup_page']);
+
+Route::post('/signup', ['uses' => 'UserController@create_user']);
+
