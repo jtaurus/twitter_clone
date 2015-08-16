d4d4effb5d93f00a85dab94b1e8db03f41491998
Jakub <truefear@gmail.com>
2015-08-16 15:46:40 +0200

    Retrieve all the neccessary data for making user profile page ( in composer )

--- app/composers.php
+++ app/composers.php
@@ -8,5 +8,12 @@ View::composer('user_profile', function($view){
 	// Find users messages:
 	$userMessages = $userReference->messages()->get();
 	$data["messages"] = $userMessages;
+	// Find user followers:
+	$userFollowers = $userReference->followers()->get();
+	$data["followers"] = $userFollowers;
+	// Find who user follows:
+	$userFollows = $userReference->follows()->get();
+	$data["follows"] = $userFollows;
+	// pass the data
 	$view->with('data', $data);
 });
