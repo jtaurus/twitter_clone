b694612c8ab037fd9df12fa0419b1a465d3dc23e
Jakub <truefear@gmail.com>
2015-08-16 15:30:58 +0200

    Create view composers file and include it.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -3,7 +3,8 @@
 class UserController extends BaseController{
 
 	public function display_user_profile($username){
-		
+		$userReference = User::where('username', '=', $username)->firstOrFail();
+		Return View::make('user_profile')->with('user', $userReference);
 	}
 
 }
